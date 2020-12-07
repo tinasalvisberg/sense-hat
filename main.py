6044c923677936435a646f5307ffaf1333f6e813
tinasalvisberg <tina.salvisberg@soverin.net>
2020-12-07 17:16:02 +0100

    notes tutorat

--- main.py
+++ main.py
@@ -129,19 +129,38 @@ one = [eins, zwei, drei, vier, five, sechs]
 two = [einsA, zweiA, dreiA, vierA, fiveA, sechsA]
 
 player = one
+scoreone = -1
+scoretwo = -1
 
 def wuerfel(player):
     zahlen = choice(player)
     sense.set_pixels(zahlen)
     sleep(6)
     sense.clear()
+    return 5
+
+def winner(scoreone, scoretwo):
+    if scoreone < scoretwo:
+        #spieler zwei gewinnt
+    elif scoreone > scoretwo:
+        #sense.show_message("win white", text_colour=w, scroll_speed=0.05)
+    else:
+        #unentschieden
 
 while True:
     orientation = sense.get_orientation_degrees()
     pitch = orientation.get("pitch")
-    print(pitch)
+    #print(pitch)
 
     if 0 <= pitch <= 40 or 320 <= pitch <= 360:
         print("nichts tun")
     else:
-        wuerfel(player)
+        if player == two:
+            player = one
+            print(scoreone)
+            #funktion für auswertung
+        else:
+            scoreone = wuerfel(player)
+            player = two
+
+#random.randint(0,5)
